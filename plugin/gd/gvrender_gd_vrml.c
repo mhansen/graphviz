19a3a815ae6956b2bfdc71674cdab63d0400b715
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-08-01 22:57:20 +0200

    plugin/gd: remove duplicate definition of Bezier in gvrender_gd_vrml.c
    Removes this warning:
    
    C:\Users\magja\graphviz\plugin\gd\gvrender_gd_vrml.c(41,86): warning C4273: 'Bezier': inconsistent dll linkage [C:\Users\magja\graphviz\plugin\gd\gvplugin_gd.vcxproj]

--- plugin/gd/gvrender_gd_vrml.c
+++ plugin/gd/gvrender_gd_vrml.c
@@ -37,8 +37,6 @@
 /* for wind() */
 #include <pathplan/pathutil.h>
 
-extern pointf Bezier(pointf * V, int degree, double t, pointf * Left, pointf * Right);
-
 typedef enum { FORMAT_VRML, } format_type;
 
 #define BEZIERSUBDIVISION 10
