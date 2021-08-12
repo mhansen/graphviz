75808f63642303a20fb8b5e5165e035fbc174a72
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-08-12 11:23:06 +0200

    gvc: add storage-class attribute declaration of gvLayoutDone in gvc.h
    This function was introduced in
    6f46c2c224180dc238851d2fb88d1fb909548376 which was part of a branch
    which initially originated before the storage-class attributes for the
    other functions in this file were introduced. This was not noted when
    this branch was rebased before merge.
    
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2058.

--- lib/gvc/gvc.h
+++ lib/gvc/gvc.h
@@ -68,7 +68,7 @@ GVC_API int gvLayout(GVC_t *gvc, graph_t *g, const char *engine);
 GVC_API int gvLayoutJobs(GVC_t *gvc, graph_t *g);
 
 /* Check if a layout has been done */
-extern bool gvLayoutDone(graph_t *g);
+GVC_API bool gvLayoutDone(graph_t *g);
 
 /* Render layout into string attributes of the graph */
 GVC_API void attach_attrs(graph_t *g);
