e03647c84831adbbb9c4020f2deb9ef6fa23ca97
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-09-25 17:09:34 -0700

    smyrna: remove 'lookup_widget' prototype
    This function was removed in 514a0056e9dfaa90d2bef63bb2150b6bd86152c9.

--- cmd/smyrna/support.h
+++ cmd/smyrna/support.h
@@ -31,19 +31,6 @@
 #endif
 
 
-/*
- * Public Functions.
- */
-
-/*
- * This function returns a widget in a component created by Glade.
- * Call it with the toplevel widget in the component (i.e. a window/dialog),
- * or alternatively any widget in the component, and the name of the widget
- * you want returned.
- */
-GtkWidget *lookup_widget(GtkWidget * widget, const gchar * widget_name);
-
-
 /* Use this function to set the directory containing installed pixmaps. */
 /* void add_pixmap_directory(const gchar * directory); */
 
