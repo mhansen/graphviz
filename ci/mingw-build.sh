eb50ded2254b45fd2fb72b2c7475ea73bc74420f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-11-26 06:41:23 +0100

    ci/mingw-build.sh: set CMAKE_OPTIONS only when using CMake

--- ci/mingw-build.sh
+++ ci/mingw-build.sh
@@ -6,8 +6,10 @@ ci/mingw-install.sh
 
 export PATH=$PATH:/c/Git/cmd
 
-# use the libs installed with pacman instead of those in
-# https://gitlab.com/graphviz/graphviz-windows-dependencies
-export CMAKE_OPTIONS=-Duse_win_pre_inst_libs=OFF
+if [ "${build_system}" = "cmake" ]; then
+    # use the libs installed with pacman instead of those in
+    # https://gitlab.com/graphviz/graphviz-windows-dependencies
+    export CMAKE_OPTIONS=-Duse_win_pre_inst_libs=OFF
+fi
 
 ci/build.sh
