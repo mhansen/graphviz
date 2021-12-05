7c019ec68bfc4d6cf280dd5353af4f51735b96ac
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-04 18:12:24 -0800

    [nfc] remove legacy sys/dir.h line from Windows config.h
    The last use of sys/dir.h was removed in
    0330a4d918dcbf6edb1ce46956a20bde9a2444f9.

--- windows/include/config.h
+++ windows/include/config.h
@@ -180,10 +180,6 @@
 /* Define to 1 if you have the <strings.h> header file. */
 //#define HAVE_STRINGS_H 1
 
-/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
-   */
-/* #undef HAVE_SYS_DIR_H */
-
 /* Define to 1 if you have the <sys/inotify.h> header file. */
 /* #undef HAVE_SYS_INOTIFY_H */
 
