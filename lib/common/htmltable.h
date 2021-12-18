9e65fb96cdc4149d24e260804c9348cbcb2a93e1
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-17 20:39:35 -0800

    remove prototype for unimplemented 'html_inside'

--- lib/common/htmltable.h
+++ lib/common/htmltable.h
@@ -171,7 +171,6 @@ extern "C" {
     extern void free_html_text(htmltxt_t *);
 
     extern boxf *html_port(node_t * n, char *pname, int* sides);
-    extern int html_inside(node_t * n, pointf p, edge_t * e);
 
 #ifdef __cplusplus
 }
