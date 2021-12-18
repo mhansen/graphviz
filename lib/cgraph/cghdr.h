f8ed6a6c6c0a577815d01991c99dcae5fac79559
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-17 20:53:16 -0800

    remove declaration of undefined 'AgAttrdisc'

--- lib/cgraph/cghdr.h
+++ lib/cgraph/cghdr.h
@@ -71,7 +71,6 @@ extern Dtdisc_t Ag_subedge_id_disc;
 extern Dtdisc_t Ag_mainedge_seq_disc;
 extern Dtdisc_t Ag_subedge_seq_disc;
 extern Dtdisc_t Ag_subgraph_id_disc;
-extern Agcbdisc_t AgAttrdisc;
 
 	/* internal constructor of graphs and subgraphs */
 Agraph_t *agopen1(Agraph_t * g);
