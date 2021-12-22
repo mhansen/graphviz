98e72ee5a6b962f608c1fb45cfa8a47984120041
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-22 08:13:03 -0800

    remove osage prototypes of unimplemented functions

--- lib/osage/osage.h
+++ lib/osage/osage.h
@@ -10,14 +10,9 @@
 
 #pragma once
 
-#include "render.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-extern void cluster_layout(Agraph_t * g);
-extern void cluster_cleanup(Agraph_t * g);
 
 #ifdef __cplusplus
 }
