ce6c4c9209a1e2dc0ad0ed0a5968270e9dcf5e0c
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-12-26 19:14:43 -0800

    handle_keypress: return a C99 bool instead of a boolean

--- plugin/xlib/gvdevice_xlib.c
+++ plugin/xlib/gvdevice_xlib.c
@@ -92,7 +92,7 @@ static void handle_client_message(GVJ_t * job, XClientMessageEvent * cmev)
         exit(0);
 }
 
-static boolean handle_keypress(GVJ_t *job, XKeyEvent *kev)
+static bool handle_keypress(GVJ_t *job, XKeyEvent *kev)
 {
     
     int i;
@@ -101,9 +101,9 @@ static boolean handle_keypress(GVJ_t *job, XKeyEvent *kev)
     keycodes = job->keycodes;
     for (i=0; i < job->numkeys; i++) {
 	if (kev->keycode == keycodes[i])
-	    return (job->keybindings[i].callback)(job);
+	    return (job->keybindings[i].callback)(job) != 0;
     }
-    return FALSE;
+    return false;
 }
 
 static Visual *find_argb_visual(Display * dpy, int scr)
