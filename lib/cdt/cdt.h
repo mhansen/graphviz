1a10388e860c465afdc20383a8f31e7885858062
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-01-01 16:42:27 +0100

    cdt: [nfc] remove whitespace in preprocessor directives in cdt.h

--- lib/cdt/cdt.h
+++ lib/cdt/cdt.h
@@ -15,13 +15,13 @@ extern "C" {
 #include <string.h>
 
 #ifdef _WIN32
-#   ifdef EXPORT_CDT
-#       define CDT_API __declspec(dllexport)
-#   else
-#       define CDT_API __declspec(dllimport)
-#   endif
+#ifdef EXPORT_CDT
+#define CDT_API __declspec(dllexport)
 #else
-#   define CDT_API /* nothing */
+#define CDT_API __declspec(dllimport)
+#endif
+#else
+#define CDT_API /* nothing */
 #endif
 
 typedef struct _dtlink_s	Dtlink_t;
