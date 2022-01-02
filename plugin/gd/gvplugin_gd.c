4bc0725bc6d14610ce1f4ab5a5a74f312e017bbe
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-01-02 19:23:28 +0100

    plugin/gd: [nfc] remove whitespace in preprocessor directives in gvplugin_gd.c

--- plugin/gd/gvplugin_gd.c
+++ plugin/gd/gvplugin_gd.c
@@ -30,9 +30,9 @@ static gvplugin_api_t apis[] = {
 };
 
 #ifdef _WIN32
-#   define GVPLUGIN_GD_API __declspec(dllexport)
+#define GVPLUGIN_GD_API __declspec(dllexport)
 #else
-#   define GVPLUGIN_GD_API
+#define GVPLUGIN_GD_API
 #endif
 
 GVPLUGIN_GD_API gvplugin_library_t gvplugin_gd_LTX_library = { "gd", apis };
