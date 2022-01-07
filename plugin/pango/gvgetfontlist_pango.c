2534a148b442dc69a0c6b481fc9920c981fdae4c
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-01-07 22:42:20 +0100

    plugin/pango: print font map when verbose for all platforms
    This effectively reverts commit
    2029d4bf76360aba04a0ca779058caba7682e1e9.
    
    Fixes this error with CMake on MinGW when compiled with -Werror:
    
    C:/Users/magja/graphviz/plugin/pango/gvgetfontlist_pango.c:477:1: error: 'printFontMap' defined but not used [-Werror=unused-function]
      477 | printFontMap (gv_font_map*gv_fmap, int sz)
          | ^~~~~~~~~~~~

--- plugin/pango/gvgetfontlist_pango.c
+++ plugin/pango/gvgetfontlist_pango.c
@@ -515,11 +515,9 @@ gv_font_map* get_font_mapping(PangoFontMap * fontmap)
     gv_flist_free_af(gv_af_p);
     agxbfree(&xb);
     agxbfree(&xb2);
-#ifndef _WIN32
     if (Verbose > 1) {
 	fprintf(stderr, "Verbose %d\n", Verbose);
 	printFontMap (gv_fmap, ps_fontnames_sz);
     }
-#endif
     return gv_fmap;
 }
