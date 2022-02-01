2c6461fbe57bf8e4e200c561f85a335d0ea5b7dc
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-02-01 12:05:53 +1100

    CMake: [nfc] uppercase 'Svgpp_*' variables
    Upcoming changes that introduce `cmake-format` checks complain that these
    identifiers do not comply with `[A-Z][0-9A-Z_]+|_[0-9a-z_]+`.

--- cmake/FindSvgpp.cmake
+++
@@ -1,6 +0,0 @@
-find_path(Svgpp_INCLUDE_DIR svgpp/svgpp.hpp)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Svgpp DEFAULT_MSG Svgpp_INCLUDE_DIR)
-
-set(Svgpp_INCLUDE_DIRS ${Svgpp_INCLUDE_DIR})
