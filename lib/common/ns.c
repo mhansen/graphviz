7c0d280b019114c57564c4cf2ac5766280447f00
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-02-25 02:07:14 +0000

    fix freeTreeList

--- lib/common/ns.c
+++ lib/common/ns.c
@@ -648,7 +648,7 @@ static void
 freeTreeList (graph_t* g)
 {
     node_t *n;
-    for (n = GD_nlist(G); n; n = ND_next(n)) {
+    for (n = GD_nlist(g); n; n = ND_next(n)) {
 	free_list(ND_tree_in(n));
 	free_list(ND_tree_out(n));
 	ND_mark(n) = FALSE;
