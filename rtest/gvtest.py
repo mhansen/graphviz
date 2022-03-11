81415d306608867466989f76242bc3d5c0d76a34
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-03-10 20:48:10 -0800

    tests: de-dupe some MinGW detection logic

--- rtest/gvtest.py
+++ rtest/gvtest.py
@@ -31,7 +31,7 @@ def compile_c(src: Path, cflags: List[str] = None, link: List[str] = None,
   if dst is None:
     _, dst = tempfile.mkstemp(".exe")
 
-  if platform.system() == "Windows" and "mingw" not in sysconfig.get_platform():
+  if platform.system() == "Windows" and not is_mingw():
     # determine which runtime library option we need
     rtflag = "-MDd" if os.environ.get("configuration") == "Debug" else "-MD"
 
