e2dbfc1a5497ba97ba301773d40b5dc8dadd64e5
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-03-11 06:53:49 -0800

    CI: install 'libANN' on macOS
    This is a dependency of lib/mingle, and transitively cmd/mingle. Note that this
    is not available through Homebrew so we need to use MacPorts.

--- ci/install-build-dependencies-macos.sh
+++ ci/install-build-dependencies-macos.sh
@@ -47,3 +47,12 @@ export CPPFLAGS="-I/usr/local/opt/qt@5/include"
 export PKG_CONFIG_PATH="/usr/local/opt/qt@5/lib/pkgconfig"
 
 brew install libxaw || brew upgrade libxaw
+
+# install MacPorts for libANN
+curl --retry 3 --location --no-progress-meter -O \
+  https://github.com/macports/macports-base/releases/download/v2.7.2/MacPorts-2.7.2-12-Monterey.pkg
+sudo installer -package MacPorts-2.7.2-12-Monterey.pkg -target /
+export PATH=/opt/local/bin:${PATH}
+
+# lib/mingle dependency
+sudo port install libANN
