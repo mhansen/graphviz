2714df1e6131b18bde6f60952b1e5150941d175d
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-04-02 09:02:31 -0700

    gml2gv: squash a -Wswitch-default warning

--- cmd/tools/gml2gv.c
+++ cmd/tools/gml2gv.c
@@ -15,6 +15,7 @@
 #include <getopt.h>
 
 #include <cgraph/exit.h>
+#include <cgraph/unreachable.h>
 
 #define N_NEW(n,t)       calloc((n),sizeof(t))
 
@@ -119,6 +120,8 @@ static void initargs(int argc, char **argv)
 			optopt);
 		usage(1);
 	    }
+	default:
+	    UNREACHABLE();
 	}
     }
 
