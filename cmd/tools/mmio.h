e5a1af7674ef4bb54140ebc279881e6dad1bba07
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-04-08 08:08:28 -0700

    mm2gv: remove unused 'mm_read_mtx_array_size'

--- cmd/tools/mmio.h
+++ cmd/tools/mmio.h
@@ -27,7 +27,6 @@ char *mm_typecode_to_str(MM_typecode matcode);
 
 int mm_read_banner(FILE * f, MM_typecode * matcode);
 int mm_read_mtx_crd_size(FILE * f, int *M, int *N, int *nz);
-int mm_read_mtx_array_size(FILE * f, int *M, int *N);
 
 int mm_write_banner(FILE * f, MM_typecode matcode);
 
