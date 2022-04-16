9f57d5bd1edca422866aab160c1f8def1a332a0b
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-04-16 15:05:30 -0700

    treat 'X_OK' as an alias of 'R_OK' in the Windows unistd.h shim
    This is not technically correct (Windows does not have `X_OK` at all), but this
    is an accurate approximation for the ways in which Graphviz uses `X_OK`. This is
    another step towards replacing lib/ast/compat_unistd.h.
    
    Gitlab: #2204

--- windows/include/unistd/unistd.h
+++ windows/include/unistd/unistd.h
@@ -19,7 +19,7 @@
    These may be OR'd together.  */
 #define R_OK    4       /* Test for read permission.  */
 #define W_OK    2       /* Test for write permission.  */
-//#define   X_OK    1   /* execute permission - unsupported in windows*/
+#define X_OK    R_OK    /* execute permission - unsupported in windows*/
 #define F_OK    0       /* Test for existence.  */
 
 #define S_ISDIR(mode) (((mode) & _S_IFDIR) == _S_IFDIR)
