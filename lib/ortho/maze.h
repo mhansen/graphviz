7f5079d3dceb075621dd41981b246e2ae2650ceb
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-04-26 08:10:04 +0300

    convert comments to doxygen format in struct maze

--- lib/ortho/maze.h
+++ lib/ortho/maze.h
@@ -64,8 +64,8 @@ typedef struct cell {
 
 typedef struct {
   int ncells, ngcells;
-  cell* cells;     /* cells not corresponding to graph nodes */
-  cell* gcells;    /* cells corresponding to graph nodes */
+  cell* cells;     ///< @brief cells not corresponding to graph nodes
+  cell* gcells;    ///< @brief cells corresponding to graph nodes
   sgraph* sg;
   Dt_t* hchans;
   Dt_t* vchans;
