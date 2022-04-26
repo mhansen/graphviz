24c9bc24c526e65734139f3c812810f47f12e4a4
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-04-26 07:28:12 +0300

    doxygen comment for maze.h

--- lib/ortho/maze.h
+++ lib/ortho/maze.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief makes @ref maze with @ref mkMaze for routing orthogonal edges
+ */
+
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
