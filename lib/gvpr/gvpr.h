2f5b03ac687d08ced5ddb93495d24e432976e0bc
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-05-02 17:51:25 +0300

    doxygen comment for lib/gvpr/gvpr.h

--- lib/gvpr/gvpr.h
+++ lib/gvpr/gvpr.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @brief graph pattern scanning and processing language API, main function @ref gvpr
+ */
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
