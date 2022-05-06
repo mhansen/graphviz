47c897b6ba73ff5eecdafe78425c4c3fd98b540f
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-05-06 10:03:07 +0300

    doxygen comment for sccmap.c

--- cmd/tools/sccmap.c
+++ cmd/tools/sccmap.c
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief extract strongly connected components of directed graphs
+ */
+
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
