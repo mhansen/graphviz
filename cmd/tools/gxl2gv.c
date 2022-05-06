05d850eab89f9624bb840b7203664697bda27ee5
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-05-06 10:01:50 +0300

    doxygen comment for gxl2gv.c

--- cmd/tools/gxl2gv.c
+++ cmd/tools/gxl2gv.c
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief <a href=https://en.wikipedia.org/wiki/GXL>GXL</a>-DOT convert subroutines
+ */
+
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
