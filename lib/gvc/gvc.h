946a9553d501ecd42477ac3cd3f5e799fb57fc6f
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-07-15 07:52:12 +0300

    Graphviz context library doxygen comments

--- lib/gvc/gvc.h
+++ lib/gvc/gvc.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Graphviz context library
+ */
+
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
