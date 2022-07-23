b8e0d5f8516e3f038d2b5f351333a3d9f88c469b
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-23 08:27:30 -0700

    sfdpgen stress_model: remove unused 'A' parameter

--- lib/sfdpgen/stress_model.h
+++ lib/sfdpgen/stress_model.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void stress_model(int dim, SparseMatrix A, SparseMatrix D, double **x, int edge_len_weighted, int maxit, double tol, int *flag);
+void stress_model(int dim, SparseMatrix D, double **x, int edge_len_weighted, int maxit, double tol, int *flag);
