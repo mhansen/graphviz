466d1810e2c391a59724db7c6d3dd571089a469f
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-08-20 19:15:57 -0700

    cgraph: Doxygen-ize and further explain 'PRISIZE_T'

--- lib/cgraph/prisize_t.h
+++ lib/cgraph/prisize_t.h
@@ -1,6 +1,19 @@
 #pragma once
 
-// `PRIu64`-alike for printing size_t
+/// \brief \p PRIu64 alike for printing \p size_t
+///
+/// Use this as:
+///
+/// \code{.c}
+///   size_t value = 42;
+///   printf("value is %" PRISIZE_T ".", value);
+///   // prints “value is 42.”
+/// \endcode
+///
+/// Note that leaving a space on either side of \p PRISIZE_T does not seem
+/// relevant in C, but if you omit this in C++ it will be interpreted as a
+/// user-defined string literal indicator. So it is best to always use a space
+/// on either side.
 #ifdef __MINGW64__
   // Microsoft’s Visual C Runtime (msvcrt) ships a printf that does not
   // understand "%zu". MSVC itself uses a different printf that does not rely on
