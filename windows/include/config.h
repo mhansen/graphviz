bd818f44cd6ac4324039d95233d16ce914567197
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-07 17:27:49 -0700

    Autotools: remove checking for the '_r' member of 'FILE'
    The last use of the result variable of this check, `HAVE_FILE_R`, was removed
    in b2f287ca315dd5bf689d9ce2ca71dbfe63fb8a61.

--- windows/include/config.h
+++ windows/include/config.h
@@ -57,9 +57,6 @@
 /* Define if FILE structure provides _next */
 /* #undef HAVE_FILE_NEXT */
 
-/* Define if FILE structure provides _r */
-/* #undef HAVE_FILE_R */
-
 /* Define if you have the fontconfig library */
 #define HAVE_FONTCONFIG 1
 
