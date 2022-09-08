4972691e6b374c376eaca167449da8a607e6f336
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-07 17:27:49 -0700

    glcomp: remove unused 'FONT_MAX_LEN'
    The last use of this was removed in ed74e24b8020a70678d05489639e0e8c4574f87d.

--- lib/glcomp/glcompdefs.h
+++ lib/glcomp/glcompdefs.h
@@ -78,10 +78,6 @@ extern "C" {
 #define	GLCOMP_DEFAULT_WIDTH	10.0f
 #define	GLCOMP_DEFAULT_HEIGHT	10.0f
 
-
-
-#define FONT_MAX_LEN                1024	/* maximum chars to draw to the screen, used for buffers also */
-
 #define C_DPI              16
 #define R_DPI              16
 
