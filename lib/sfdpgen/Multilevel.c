282458b1560318104a21013d238d201844fa125e
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-09 20:13:55 -0700

    sfdpgen maximal_independent_edge_set_heavest…: use cgraph wrapper for allocation
    The lib/cgraph/alloc.h wrappers are similar to the older lib/common/memory.h
    wrappers except (1) they are header-only and (2) they live in a directory
    (cgraph) that is at the root of the dependency tree. The long term plan is to
    replace all use of lib/common/memory.h with lib/cgraph/alloc.h.

--- lib/sfdpgen/Multilevel.c
+++ lib/sfdpgen/Multilevel.c
@@ -741,7 +741,7 @@ static void maximal_independent_edge_set_heavest_edge_pernode_scaled(SparseMatri
   m = A->m;
   n = A->n;
   assert(n == m);
-  *matching = N_GNEW(m,int);
+  *matching = gv_calloc(m, sizeof(int));
   for (i = 0; i < m; i++) (*matching)[i] = i;
   *nmatch = n;
 
