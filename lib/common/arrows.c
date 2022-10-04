dad7d7fe3e5a11442701f20aea40143f62fe8757
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-10-04 13:40:45 +0200

    arrows: arrow_length_box: take edge penwidth into account when calculating length
    The test_edge_node_overlap_box_edge_arrow test case now succeeds, so
    the expectancy that it should fail is removed.
    
    Towards https://gitlab.com/graphviz/graphviz/-/issues/372.

--- lib/common/arrows.c
+++ lib/common/arrows.c
@@ -124,12 +124,13 @@ static pointf arrow_type_gap(GVJ_t * job, pointf p, pointf u, double arrowsize, 
 
 static double arrow_length_generic(double lenfact, double arrowsize, double penwidth, int flag);
 static double arrow_length_normal(double lenfact, double arrowsize, double penwidth, int flag);
+static double arrow_length_box(double lenfact, double arrowsize, double penwidth, int flag);
 
 static const arrowtype_t Arrowtypes[] = {
     {ARR_TYPE_NORM, 1.0, arrow_type_normal, arrow_length_normal},
     {ARR_TYPE_CROW, 1.0, arrow_type_crow, arrow_length_generic},
     {ARR_TYPE_TEE, 0.5, arrow_type_tee, arrow_length_generic},
-    {ARR_TYPE_BOX, 1.0, arrow_type_box, arrow_length_generic},
+    {ARR_TYPE_BOX, 1.0, arrow_type_box, arrow_length_box},
     {ARR_TYPE_DIAMOND, 1.2, arrow_type_diamond, arrow_length_generic},
     {ARR_TYPE_DOT, 0.8, arrow_type_dot, arrow_length_generic},
     {ARR_TYPE_CURVE, 1.0, arrow_type_curve, arrow_length_generic},
@@ -982,4 +983,15 @@ static double arrow_length_normal(double lenfact, double arrowsize,
   // arrow length is the x value of the start point since the arrow points along
   // the positive x axis and ends at origin
   return full_length - overlap;
+}
+
+static double arrow_length_box(double lenfact, double arrowsize,
+			       double penwidth, int flag) {
+    (void)flag;
+
+    // The `box` arrow shape begins with a polyline which doesn't extend
+    // visually beyond its starting point, so we only have to take penwidth
+    // into account at the end point.
+
+    return lenfact * arrowsize * ARROW_LENGTH + penwidth / 2;
 }
