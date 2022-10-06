290d702b24441a3455ff83df9bc47ce2a5be2ff0
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2022-10-06 11:01:37 +0200

    tests: test_edge_node_overlap_utilities: adjust max_edge_stem_arrow_overlap for 'diamond' arrow

--- tests/test_edge_node_overlap_utilities.cpp
+++ tests/test_edge_node_overlap_utilities.cpp
@@ -446,8 +446,11 @@ void test_edge_node_overlap(const graph_options &graph_options,
            // FIXME: adjust this when `curve` is fixed for penwidth
            graph_options.edge_penwidth / 2 + graphviz_bezier_clip_margin},
           {"diamond",
-           // FIXME: adjust this when `diamond` is fixed for penwidth
-           graph_options.edge_penwidth / 2 + graphviz_bezier_clip_margin},
+           [&]() { // FIXME: calculate this accurately for diamond arrow
+             const auto tip_scale = 1.8027756377319939; // empirical value
+             return graph_options.edge_penwidth / 2 * tip_scale +
+                    graphviz_bezier_clip_margin;
+           }()},
           {"dot",
            graph_options.edge_penwidth / 2 + graphviz_bezier_clip_margin},
           {"icurve",
