8a2e20de31f4bb913067af2322a067ed8f9360b8
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-10-09 13:12:07 -0700

    common: remove unused 'PI' constant

--- lib/common/ellipse.c
+++ lib/common/ellipse.c
@@ -57,8 +57,6 @@
 #define MAX(a,b)        ((a)>(b)?(a):(b))
 #define MIN(a,b)        ((a)<(b)?(a):(b))
 
-#define PI            3.14159265358979323846
-
 typedef struct {
     double x, y;
 } pointf;
