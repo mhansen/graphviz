a6ed4358d4673a9acdb73a4b074721d8a57bc240
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-10-10 18:36:03 -0700

    gvc: remove unused 'gvNEWargvlist'
    This seems to have never been used.

--- lib/gvc/gvjobs.c
+++ lib/gvc/gvjobs.c
@@ -100,11 +100,6 @@ GVJ_t *gvjobs_next(GVC_t * gvc)
     return (gvc->job = job);
 }
 
-gv_argvlist_t *gvNEWargvlist(void)
-{
-    return zmalloc(sizeof(gv_argvlist_t));
-}
-
 void gv_argvlist_set_item(gv_argvlist_t *list, int index, char *item)
 {
     if (index >= list->alloc) {
