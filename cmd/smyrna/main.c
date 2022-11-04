mhansen/graphviz

commit 67f88eb86c9966daa4d8c3a6f25aee8cd35a046d

CMake: enable 'smyrna'
Gitlab: #1836, #1854

CMakeLists.txt
@@ -71,6 +71,7 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
   set(MAN_INSTALL_DIR     "${CMAKE_INSTALL_MANDIR}")
   set(libdir              "${CMAKE_INSTALL_FULL_LIBDIR}")
   set(includedir          "${CMAKE_INSTALL_FULL_INCLUDEDIR}")
+  set(DATA_INSTALL_DIR    "${CMAKE_INSTALL_FULL_DATAROOTDIR}")
 else()
   set(BINARY_INSTALL_DIR  bin)
   set(LIBRARY_INSTALL_DIR lib)
@@ -78,6 +79,7 @@ else()
   set(MAN_INSTALL_DIR     share/man)
   set(libdir              "${CMAKE_INSTALL_PREFIX}/${LIBRARY_INSTALL_DIR}")
   set(includedir          "${CMAKE_INSTALL_PREFIX}/include")
+  set(DATA_INSTALL_DIR    "${CMAKE_INSTALL_PREFIX}/share")
 endif()
 
 set(PLUGIN_INSTALL_DIR      ${LIBRARY_INSTALL_DIR}/graphviz)
@@ -283,6 +285,7 @@ add_subdirectory(contrib/diffimg)
 add_subdirectory(lib)
 add_subdirectory(plugin)
 add_subdirectory(cmd)
+add_subdirectory(share)
 if(with_cxx_tests)
   add_subdirectory(tests)
 endif()

cmd/smyrna/CMakeLists.txt
@@ -1,0 +1,150 @@
+if(with_smyrna)
+
+  if(with_expat)
+    find_package(EXPAT REQUIRED)
+  endif()
+  find_package(Freetype REQUIRED)
+  find_package(GLUT REQUIRED)
+  find_package(GTK2 REQUIRED COMPONENTS gtk glade)
+  find_package(PkgConfig REQUIRED)
+  pkg_check_modules(FONTCONFIG REQUIRED fontconfig)
+  pkg_check_modules(GTKGLEXT REQUIRED gtkglext-1.0)
+  pkg_check_modules(GTS REQUIRED gts)
+  pkg_check_modules(XRENDER REQUIRED xrender)
+
+  add_definitions(-DSMYRNA_PATH="${DATA_INSTALL_DIR}/graphviz/smyrna")
+
+  add_executable(smyrna
+    arcball.h
+    draw.h
+    glexpose.h
+    glmotion.h
+    gltemplate.h
+    glutrender.h
+    gui/appmouse.h
+    gui/callbacks.h
+    gui/datalistcallbacks.h
+    gui/frmobjectui.h
+    gui/glcompui.h
+    gui/gui.h
+    gui/menucallbacks.h
+    gui/toolboxcallbacks.h
+    gui/topviewsettings.h
+    gvprpipe.h
+    hier.h
+    hotkeymap.h
+    polytess.h
+    selectionfuncs.h
+    smyrna_utils.h
+    smyrnadefs.h
+    support.h
+    topfisheyeview.h
+    topviewfuncs.h
+    tvnodes.h
+    viewport.h
+    viewportcamera.h
+
+    arcball.c
+    draw.c
+    glexpose.c
+    glmotion.c
+    gltemplate.c
+    gui/appmouse.c
+    gui/callbacks.c
+    gui/datalistcallbacks.c
+    gui/frmobjectui.c
+    gui/glcompui.c
+    gui/gui.c
+    gui/menucallbacks.c
+    gui/toolboxcallbacks.c
+    gui/topviewsettings.c
+    glutrender.c
+    gvprpipe.c
+    hier.c
+    hotkeymap.c
+    main.c
+    polytess.c
+    selectionfuncs.c
+    smyrna_utils.c
+    topfisheyeview.c
+    topviewfuncs.c
+    tvnodes.c
+    viewport.c
+    viewportcamera.c
+  )
+
+  target_include_directories(smyrna PRIVATE
+    ${CMAKE_CURRENT_SOURCE_DIR}
+    ${GRAPHVIZ_LIB_DIR}
+    ${GRAPHVIZ_LIB_DIR}/cdt
+    ${GRAPHVIZ_LIB_DIR}/cgraph
+    ${GRAPHVIZ_LIB_DIR}/common
+    ${GRAPHVIZ_LIB_DIR}/gvc
+    ${GRAPHVIZ_LIB_DIR}/gvpr
+    ${GRAPHVIZ_LIB_DIR}/pathplan
+    ${GRAPHVIZ_LIB_DIR}/xdot
+    gui
+  )
+
+  target_include_directories(smyrna SYSTEM PRIVATE
+    ${EXPAT_INCLUDE_DIRS}
+    ${FONTCONFIG_INCLUDE_DIRS}
+    ${Freetype_INCLUDE_DIRS}
+    ${GLUT_INCLUDE_DIRS}
+    ${GTK2_INCLUDE_DIRS}
+    ${GTKGLEXT_INCLUDE_DIRS}
+    ${GTS_INCLUDE_DIRS}
+    ${XRENDER_INCLUDE_DIRS}
+  )
+
+  target_link_libraries(smyrna
+    cdt
+    cgraph
+    expr
+    glcomp
+    gvc
+    gvpr
+    ingraphs
+    neatogen
+    pathplan
+    topfish
+    xdot
+    ${EXPAT_LIBRARIES}
+    ${FONTCONFIG_LIBRARIES}
+    ${Freetype_LIBRARIES}
+    ${GLUT_LIBRARIES}
+    ${GTK2_LIBRARIES}
+    ${GTKGLEXT_LIBRARIES}
+    ${GTS_LIBRARIES}
+    ${MATH_LIB}
+    ${XRENDER_LIBRARIES}
+  )
+
+  install(
+    TARGETS smyrna
+    RUNTIME DESTINATION ${BINARY_INSTALL_DIR}
+    LIBRARY DESTINATION ${LIBRARY_INSTALL_DIR}
+    ARCHIVE DESTINATION ${LIBRARY_INSTALL_DIR}
+  )
+
+  if(GZIP)
+    add_custom_target(man-smyrna ALL DEPENDS smyrna.1.gz
+                      COMMENT "smyrna man page")
+    add_custom_command(
+      OUTPUT smyrna.1.gz
+      COMMAND ${GZIP} -9 --no-name --to-stdout smyrna.1
+        >"${CMAKE_CURRENT_BINARY_DIR}/smyrna.1.gz"
+      MAIN_DEPENDENCY smyrna.1
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      COMMENT "compress smyrna man page")
+    install(
+      FILES ${CMAKE_CURRENT_BINARY_DIR}/smyrna.1.gz
+      DESTINATION ${MAN_INSTALL_DIR}/man1)
+  else()
+    install(
+      FILES smyrna.1
+      DESTINATION ${MAN_INSTALL_DIR}/man1
+    )
+  endif()
+
+endif()

cmd/CMakeLists.txt
@@ -2,4 +2,5 @@ add_subdirectory(dot)
 add_subdirectory(edgepaint)
 add_subdirectory(gvpr)
 add_subdirectory(mingle)
+add_subdirectory(smyrna)
 add_subdirectory(tools)

share/CMakeLists.txt
@@ -1,0 +1,2 @@
+add_subdirectory(examples)
+add_subdirectory(gui)

share/examples/CMakeLists.txt
@@ -1,0 +1,9 @@
+if(with_smyrna)
+
+  install(
+    FILES
+      4elt.gv
+      world.gv
+    DESTINATION ${DATA_INSTALL_DIR}/graphviz/examples)
+
+endif()

share/gui/CMakeLists.txt
@@ -1,0 +1,38 @@
+if(with_smyrna)
+
+  install(
+    FILES
+      2D.png
+      3D.png
+      arial.tga
+      attr_widgets.dot
+      attrs.txt
+      center.png
+      cursor.png
+      details.png
+      fisheye.png
+      fisheye.raw
+      fit_to_screen.png
+      mod_3D.png
+      mod_fisheye.png
+      mouse_actions.txt
+      move.png
+      new_delete.png
+      no_fisheye.png
+      pan.png
+      pan.raw
+      selectr.png
+      selectx.png
+      single_select.png
+      smyrna.glade
+      template.dot
+      zoom.png
+      zoom.png
+      zoom.raw
+      zoom_in.png
+      zoom_out.png
+      zoomin.png
+      zoomout.png
+    DESTINATION ${DATA_INSTALL_DIR}/graphviz/smyrna)
+
+endif()

commit 24bd92c1e5d49b354141cd06d88ca658991b9825

fix installing example graphs during CPack; rewrite 'SMYRNA_PATH' discovery
This is effectively two commits in one. But there seems to be no way to separate
these changes without the parts individually failing
tests/test_tools.py::test_tools[smyrna]. Smyrna auxiliary files were being
installed incorrectly prior to this commit, so installing them correctly and
fixing how Smyrna finds them needs to be done in a single change.

---

CMake: fix installing example graphs during CPack

When running `cpack`, instead of staging the Smyrna auxiliary files (share/**)
and the example graphs (graphs/**) in build-local directories, they would be
incorrectly staged into their final install paths. This was quite unexpected as
it would result in errors if you did not have permission to write to the install
directories or deleting+replacing files from a previous Graphviz installation if
you did.

This bug was introduced in 67f88eb86c9966daa4d8c3a6f25aee8cd35a046d as an
accidental side effect of enabling Smyrna in the CMake build system. A close
re-reading of the CPack¹ and `GNUInstallDirs`² docs suggests that absolute paths
should never be used in `install` rules:

  `CMAKE_INSTALL_<dir>`
    Destination for files of a given type. This value may be passed to the
    `DESTINATION` options of `install()` commands for the corresponding file
    type. It should typically be a path relative to the installation prefix so
    that it can be converted to an absolute path in a relocatable way (see
    `CMAKE_INSTALL_FULL_<dir>`). However, an absolute path is also allowed.

  `CMAKE_INSTALL_FULL_<dir>`
    The absolute path generated from the corresponding `CMAKE_INSTALL_<dir>`
    value. If the value is not already an absolute path, an absolute path is
    constructed typically by prepending the value of the `CMAKE_INSTALL_PREFIX`
    variable. However, there are some special cases as documented below.

This change brings the behavior on Linux in line with how CPack operates on

CMakeLists.txt
@@ -64,7 +64,7 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
   set(MAN_INSTALL_DIR     "${CMAKE_INSTALL_MANDIR}")
   set(libdir              "${CMAKE_INSTALL_FULL_LIBDIR}")
   set(includedir          "${CMAKE_INSTALL_FULL_INCLUDEDIR}")
-  set(DATA_INSTALL_DIR    "${CMAKE_INSTALL_FULL_DATAROOTDIR}")
+  set(DATA_INSTALL_DIR    "${CMAKE_INSTALL_DATAROOTDIR}")
 else()
   set(BINARY_INSTALL_DIR  bin)
   set(LIBRARY_INSTALL_DIR lib)

cmd/smyrna/CMakeLists.txt
@@ -12,8 +12,6 @@ if(with_smyrna)
   pkg_check_modules(GTS REQUIRED gts)
   pkg_check_modules(XRENDER REQUIRED xrender)
 
-  add_definitions(-DSMYRNA_PATH="${DATA_INSTALL_DIR}/graphviz/smyrna")
-
   add_executable(smyrna
     arcball.h
     draw.h

cmd/smyrna/Makefile.am
@@ -1,7 +1,6 @@
 ## Process this file with automake to produce Makefile.in
 
 AM_CPPFLAGS = \
-	-DSMYRNA_PATH='"$(pkgdatadir)/smyrna"' \
 	-I$(top_srcdir)/lib \
 	-I$(top_srcdir)/lib/cgraph \
 	-I$(top_srcdir)/lib/cdt \

cmd/smyrna/main.c
@@ -36,7 +36,23 @@
 #include "glutrender.h"
 
 #include <getopt.h>
-#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
+#ifdef __FreeBSD__
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#endif
+
+#if !defined(__APPLE__) && !defined(_WIN32)
+#include <unistd.h>
+#endif
 
 static char *smyrnaDir;		/* path to directory containin smyrna data files */
 static char *smyrnaGlade;
@@ -168,49 +184,153 @@ static void windowedMode(int argc, char *argv[])
 	gtk_main();
 }
 
+#if !defined(__APPLE__) && !defined(_WIN32)
+/// read the given symlink in the /proc file system
+static char *read_proc(const char *path) {
 
+  char buf[PATH_MAX + 1] = {0};
+  if (readlink(path, buf, sizeof(buf)) < 0)
+    return NULL;
 
+  // was the path too long?
+  if (buf[sizeof(buf) - 1] != '\0')
+    return NULL;
+
+  return gv_strdup(buf);
+}
+#endif
+
+/// find an absolute path to the current executable
+static char *find_me(void) {
+
+  // macOS
+#ifdef __APPLE__
+  {
+    // determine how many bytes we will need to allocate
+    uint32_t buf_size = 0;
+    int rc = _NSGetExecutablePath(NULL, &buf_size);
+    assert(rc != 0);
+    assert(buf_size > 0);
+
+    path = gv_alloc(buf_size);
+
+    // retrieve the actual path
+    if (_NSGetExecutablePath(path, &buf_size) < 0) {
+      fprintf(stderr, "failed to get path for executable.\n");
+      graphviz_exit(EXIT_FAILURE);
+    }
+
+    // try to resolve any levels of symlinks if possible
+    while (true) {
+      char buf[PATH_MAX + 1] = {0};
+      if (readlink(path, buf, sizeof(buf)) < 0)
+        return path;
+
+      free(path);
+      path = gv_strdup(buf);
+    }
+  }
+#elif defined(_WIN32)
+  {
+    char *path = NULL;
+    size_t path_size = 0;
+    int rc = 0;
+
+    do {
+      size_t size = path_size == 0 ? 1024 : (path_size * 2);
+      path = gv_realloc(path, path_size, size);
+      path_size = size;
+
+      rc = GetModuleFileName(NULL, path, path_size);
+      if (rc == 0) {
+        fprintf(stderr, "failed to get path for executable.\n");
+        graphviz_exit(EXIT_FAILURE);
+      }
+
+    } while (rc == path_size);
+
+    return path;
+  }
+#else
+
+  // Linux
+  char *path = read_proc("/proc/self/exe");
+  if (path != NULL)
+    return path;
+
+  // DragonFly BSD, FreeBSD
+  path = read_proc("/proc/curproc/file");
+  if (path != NULL)
+    return path;
+
+  // NetBSD
+  path = read_proc("/proc/curproc/exe");
+  if (path != NULL)
+    return path;
+
+// /proc-less FreeBSD
+#ifdef __FreeBSD__
+  {
+    int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
+    static const size_t MIB_LENGTH = sizeof(mib) / sizeof(mib[0]);
+    char buf[PATH_MAX + 1] = {0};
+    size_t buf_size = sizeof(buf);
+    if (sysctl(mib, MIB_LENGTH, buf, &buf_size, NULL, 0) == 0)
+      return gv_strdup(buf);
+  }
+#endif
+#endif
+
+  fprintf(stderr, "failed to get path for executable.\n");
+  graphviz_exit(EXIT_FAILURE);
+}
+
+/// find an absolute path to where Smyrna auxiliary files are stored
+static char *find_share(void) {
+
+#ifdef _WIN32
+  const char PATH_SEPARATOR = '\\';
+#else
+  const char PATH_SEPARATOR = '/';
+#endif
+
+  // find the path to the `smyrna` binary
+  char *smyrna_exe = find_me();
+
+  // assume it is of the form …/bin/smyrna[.exe] and construct
+  // …/share/graphviz/smyrna
+
+  char *slash = strrchr(smyrna_exe, PATH_SEPARATOR);
+  if (slash == NULL) {
+    fprintf(stderr, "no path separator in path to self, %s\n", smyrna_exe);
+    free(smyrna_exe);
+    graphviz_exit(EXIT_FAILURE);
+  }
+
+  *slash = '\0';
+  slash = strrchr(smyrna_exe, PATH_SEPARATOR);
+  if (slash == NULL) {
+    fprintf(stderr, "no path separator in directory containing self, %s\n",
+            smyrna_exe);
+    free(smyrna_exe);
+    graphviz_exit(EXIT_FAILURE);
+  }
+
+  *slash = '\0';
+  size_t share_len = strlen(smyrna_exe) + strlen("/share/graphviz/smyrna") + 1;
+  char *share = gv_alloc(share_len);
+  snprintf(share, share_len, "%s%cshare%cgraphviz%csmyrna", smyrna_exe,
+           PATH_SEPARATOR, PATH_SEPARATOR, PATH_SEPARATOR);
+  free(smyrna_exe);
+
+  return share;
+}
 
 int main(int argc, char *argv[])
 {
     smyrnaDir = getenv("SMYRNA_PATH");
     if (!smyrnaDir) {
-#ifdef _WIN32
-#define BSZ 1024
-#define SMYRNA "\\share\\graphviz\\smyrna"
-
-	int r;
-	char line[BSZ];
-	char* s;
-
-	r = GetModuleFileName(NULL, line, BSZ);
-	if (!r || (r == BSZ)) {
-	    fprintf (stderr,"failed to get path for executable.\n");
-	    graphviz_exit(1);
-	}
-
-	/* line contains path to smyrna: "$GVROOT\bin\smyrna.exe" 
-	 * We want to obtain $GVROOT. We find the second rightmost '\'
-	 * and store a '\0' there.
- 	 */
-	s = strrchr(line,'\\');
-	if (!s) {
-	    fprintf (stderr,"no backslash in path %s.\n", line);
-	    graphviz_exit(1);
-	}
-	while ((s != line) && (*(--s) != '\\')) ;
-	if (s == line) {
-	    fprintf (stderr,"no backslash in path %s.\n", line);
-	    graphviz_exit(1);
-	}
-	*s = '\0';
-
-	smyrnaDir = gv_calloc(strlen(line) + sizeof(SMYRNA), sizeof(char));
-	strcpy (smyrnaDir, line);
-	strcat(smyrnaDir, SMYRNA);
-#else
-	smyrnaDir = SMYRNA_PATH;
-#endif
+	smyrnaDir = find_share();
     }
     load_attributes();
 