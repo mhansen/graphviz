3fb03f82190ad4ddb4c4a221cd0356c16b14d938
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-12-20 21:11:53 -0800

    symrna: squash -Wunused-parameter warnings in data callbacks

--- cmd/smyrna/gui/datalistcallbacks.c
+++ cmd/smyrna/gui/datalistcallbacks.c
@@ -13,30 +13,48 @@
 
 void btnTVOK_clicked_cb(GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     gtk_widget_hide(glade_xml_get_widget(xml, "frmTVNodes"));
 }
 
 void on_TVNodes_close (GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     gtk_widget_hide(glade_xml_get_widget(xml, "frmTVNodes"));
 }
 
 void btnTVShowAll_clicked_cb(GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     tv_show_all();
 }
 
 void btnTVHideAll_clicked_cb(GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     tv_hide_all();
 }
 
 void btnTVSaveAs_clicked_cb(GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     tv_save_as(0);
 }
 
 void btnTVSaveWith_clicked_cb(GtkWidget * widget, gpointer user_data)
 {
+    (void)widget;
+    (void)user_data;
+
     tv_save_as(1);
 }
