a031f7f778d903e508a44a30c5194ea9dd91c5b2
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-12-20 21:44:47 -0800

    smyrna: remove unused 'FontSize'

--- cmd/smyrna/viewport.c
+++ cmd/smyrna/viewport.c
@@ -165,8 +165,6 @@ void set_viewport_settings_from_template(ViewInfo *vi, Agraph_t *g) {
     /*default line width */
     vi->LineWidth =
 	(float) atof(get_attribute_value("defaultlinewidth", vi, g));
-    vi->FontSize =
-	(float) atof(get_attribute_value("defaultfontsize", vi, g));
 
     vi->mg.width =
 	atoi(get_attribute_value("defaultmagnifierwidth", vi, g));
@@ -327,7 +325,6 @@ void init_viewport(ViewInfo * view)
 
 
     view->zoom = -20;
-    view->FontSize = 52;
 
     view->mg.active = 0;
     view->mg.x = 0;
