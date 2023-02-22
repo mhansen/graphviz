9feb3968a9d865c97d104846578145eae1db17a3
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-02-22 12:28:46 -0800

    python -m black doc/infosrc/jconvert.py
    Gitlab: #2347

--- doc/infosrc/jconvert.py
+++ doc/infosrc/jconvert.py
@@ -10,16 +10,18 @@ import json
 from json2html import json2html
 
 
-def main(): # pylint: disable=missing-function-docstring
-  parser = argparse.ArgumentParser(description=__doc__)
-  parser.add_argument("input", type=argparse.FileType("rt"), help="Input file")
-  parser.add_argument("output", type=argparse.FileType("wt"), help="Output file")
-  args = parser.parse_args()
+def main():  # pylint: disable=missing-function-docstring
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("input", type=argparse.FileType("rt"), help="Input file")
+    parser.add_argument("output", type=argparse.FileType("wt"), help="Output file")
+    args = parser.parse_args()
 
-  json_input = json.load(args.input)
-  html_output = json2html.convert(json=json_input, table_attributes='class="jsontable"')
-  args.output.write(html_output)
+    json_input = json.load(args.input)
+    html_output = json2html.convert(
+        json=json_input, table_attributes='class="jsontable"'
+    )
+    args.output.write(html_output)
 
 
 if __name__ == "__main__":
-  main()
+    main()
