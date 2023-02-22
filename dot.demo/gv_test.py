b8efd3a729ad2b1f97f72a11067e5015f6115c07
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-02-22 12:29:15 -0800

    python -m black dot.demo/gv_test.py
    Gitlab: #2347

--- dot.demo/gv_test.py
+++ dot.demo/gv_test.py
@@ -4,12 +4,12 @@
 example usage of Graphviz Python module
 """
 
-import gv # pylint: disable=import-error
+import gv  # pylint: disable=import-error
 
 g = gv.digraph("G")
-n = gv.node(g,"hello")
-m = gv.node(g,"world")
-e = gv.edge(n,m)
+n = gv.node(g, "hello")
+m = gv.node(g, "world")
+e = gv.edge(n, m)
 gv.layout(g, "dot")
 gv.render(g, "png", "gv_test.png")
 gv.rm(g)
