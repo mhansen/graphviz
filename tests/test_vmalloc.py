907271a7a427218c6795b68dd382254a8cc25140
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-02-22 12:33:43 -0800

    python -m black tests/test_vmalloc.py
    Gitlab: #2347

--- tests/test_vmalloc.py
+++ tests/test_vmalloc.py
@@ -6,21 +6,22 @@ import platform
 import sys
 
 sys.path.append(os.path.dirname(__file__))
-from gvtest import run_c #pylint: disable=wrong-import-position
+from gvtest import run_c  # pylint: disable=wrong-import-position
+
 
 def test_vmalloc():
-  """run the vmalloc unit tests"""
+    """run the vmalloc unit tests"""
 
-  # locate the vmalloc unit tests
-  src = Path(__file__).parent.resolve() / "../lib/vmalloc/test.c"
-  assert src.exists()
+    # locate the vmalloc unit tests
+    src = Path(__file__).parent.resolve() / "../lib/vmalloc/test.c"
+    assert src.exists()
 
-  # locate lib directory that needs to be in the include path
-  lib = Path(__file__).parent.resolve() / "../lib"
+    # locate lib directory that needs to be in the include path
+    lib = Path(__file__).parent.resolve() / "../lib"
 
-  # extra C flags this compilation needs
-  cflags = ["-I", lib]
-  if platform.system() != "Windows":
-    cflags += ["-std=gnu99", "-Wall", "-Wextra", "-Werror"]
+    # extra C flags this compilation needs
+    cflags = ["-I", lib]
+    if platform.system() != "Windows":
+        cflags += ["-std=gnu99", "-Wall", "-Wextra", "-Werror"]
 
-  _, _ = run_c(src, cflags=cflags)
+    _, _ = run_c(src, cflags=cflags)
