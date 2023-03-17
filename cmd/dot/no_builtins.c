be8367708e65326d2637a27d9bb1b18ec58d8d1e
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-03-16 17:28:27 -0700

    dot: clang-format no_builtins.c
    This file was close enough to compliant already.

--- cmd/dot/no_builtins.c
+++ cmd/dot/no_builtins.c
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property 
+ * Copyright (c) 2011 AT&T Intellectual Property
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,5 +10,4 @@
 
 #include <gvc/gvplugin.h>
 
-lt_symlist_t lt_preloaded_symbols[] = { { 0, 0 } };
-
+lt_symlist_t lt_preloaded_symbols[] = {{0, 0}};
