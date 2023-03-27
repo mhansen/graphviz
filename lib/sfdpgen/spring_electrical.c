56aa1e7532d61e983b374dae32c25065fd57c115
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-03-27 12:33:59 -0700

    sfdpgen: remove unused 'METHOD_STRESS_MAXENT'

--- lib/sfdpgen/spring_electrical.c
+++ lib/sfdpgen/spring_electrical.c
@@ -75,7 +75,7 @@ static char* tschemes[] = {
 };
 
 static char* methods[] = {
-  "SPRING_ELECTRICAL", "SPRING_MAXENT", "STRESS_MAXENT"
+  "SPRING_ELECTRICAL", "SPRING_MAXENT"
 };
 
 void spring_electrical_control_print(spring_electrical_control ctrl){
